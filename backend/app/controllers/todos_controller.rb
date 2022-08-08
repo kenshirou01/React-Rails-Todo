b38ac7ceedd4cd57f2beaeae9941ef050df7ae01
kenshirou01 <rush2065kingcrab0120@gmail.com>
2022-08-09 00:15:42 +0900

    first commit

---
+++ backend/app/controllers/todos_controller.rb
@@ -0,0 +1,5 @@
+class TodosController < ApplicationController
+  def index
+    @message = 'こんにちは'
+  end
+end
---
+++ frontend/app/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <>
+      <h1>Scuba Diving Spots ~ Home ~</h1>
+    </>
+  );
+}
